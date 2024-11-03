d73055e06b170edf3bb0660936b0496d91039e08
Josineudo Arruda <96135176+josineudo-arruda@users.noreply.github.com>
2024-11-03 15:58:01 -0300

    commit inicial

---
+++ evoflui-backend/src/main/java/com/proact/evoflui_backend/EvofluiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.proact.evoflui_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvofluiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvofluiBackendApplication.class, args);
+	}
+
+}
